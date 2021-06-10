06fe76bd34a00ebe6d89eb7aee4258da0bf08b70
Artem <53917645+under-web@users.noreply.github.com>
2021-06-10 13:02:32 +0300

    find solved .get_attribute('aria-label')

--- imitation_code.py
+++ imitation_code.py
@@ -2,15 +2,17 @@ import time
 from selenium import webdriver
 
 
-url = 'https://www.google.com/maps/place/%D0%9E%D0%B0%D0%B7%D0%B8%D1%81/@55.8029471,49.0908432,13z/data=!4m10!1m3!2m2!1z0JrQsNC30LDQvdGMINGB0YPQstC10L3QuNGA0Ys!6e6!3m5!1s0x415ead532abb0673:0xde6d1e23530868b1!8m2!3d55.826235!4d49.1005619!15sCh3QmtCw0LfQsNC90Ywg0YHRg9Cy0LXQvdC40YDRi1ofIh3QutCw0LfQsNC90Ywg0YHRg9Cy0LXQvdC40YDRi5IBDWdyb2Nlcnlfc3RvcmU?hl=ru-RU'
+url = 'https://www.google.com/maps/search/Казань+сувениры/@55.802957,49.0908432,13z/data=!3m1!4b1?hl=ru-RU'
 driver = webdriver.Firefox()
 driver.maximize_window() # For maximizing window
 driver.implicitly_wait(20)
 driver.get(url)
 time.sleep(3)
-site = driver.find_element_by_css_selector('.HY5zDd')
+site = driver.find_elements_by_class_name('a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd')
+for i in site:
+    print(i.get_attribute('aria-label'))
 
 print(site.text)
 
 driver.quit()
-# <div jstcache="319" class="QSFF4-text gm2-body-2" jsan="7.QSFF4-text,7.gm2-body-2">oasiscatalog.com</div>
+# <a aria-label="Сувениры" jsaction="pane.wfvdle5;focus:pane.wfvdle5;blur:pane.wfvdle5;auxclick:pane.wfvdle5;contextmenu:pane.wfvdle5;keydown:pane.wfvdle5;clickmod:pane.wfvdle5" jstcache="127" href="https://www.google.com/maps/place/%D0%A1%D1%83%D0%B2%D0%B5%D0%BD%D0%B8%D1%80%D1%8B/data=!4m5!3m4!1s0x415ead1060310785:0xd8da41046261f9e3!8m2!3d55.788186!4d49.1204689?authuser=0&amp;hl=ru&amp;rclk=1" class="a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd" jsan="7.a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd,0.aria-label,8.href,0.jsaction"></a>

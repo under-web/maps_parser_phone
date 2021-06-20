574c0893f76f61f0ffad6514731396b49cbdcac0
Artem <53917645+under-web@users.noreply.github.com>
2021-06-20 21:14:29 +0300

    add export in csv beta 2.0

--- parser_google_org.py
+++ parser_google_org.py
@@ -1,10 +1,11 @@
+import csv
+import re
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 
 list_urls = []
-
 
 def get_town_in_file():
     """
@@ -13,7 +14,7 @@ def get_town_in_file():
     """
     with open('town.txt', 'r', encoding='utf-8') as file_town:
         town = file_town.read()
-        return town
+        return town.strip()
 
 
 def get_categories_in_file():
@@ -23,7 +24,7 @@ def get_categories_in_file():
     """
     with open('categories.txt', 'r', encoding='utf-8') as catg_town:
         categories = catg_town.read()
-        return categories
+        return categories.strip()
 
 
 def run_browser(town, categories):
@@ -38,6 +39,7 @@ def run_browser(town, categories):
     opts.headless = True
     assert opts.headless
     browser = webdriver.Firefox(options=opts)
+    # browser = webdriver.Firefox()
 
     base_url = 'https://www.google.com/maps/search/' + town + '+' + categories + '/@55.802957,49.0908432,13z/data=!3m1!4b1?hl=ru-RU'
     browser.get(base_url)
@@ -69,15 +71,22 @@ def run_browser(town, categories):
             browser.close()
             browser.quit()
             return list_urls
-        # browser.close()
+        browser.close()
         browser.quit()
         return list_urls
 
 
-def save_writer(nam):
-    name_file = get_town_in_file() + '.txt'
-    with open(f'{name_file}', 'a', encoding='utf-8', errors='ignore') as file:
-        file.write(nam + '\n\n' + '-----------------------------------------------------------' + '\n\n')
+def save_in_csv(out_data):
+    with open("classmates.csv", mode="a", encoding='utf-8') as csv_file:
+        file_writer = csv.writer(csv_file, delimiter=";")
+        file_writer.writerow(out_data)
+
+
+#
+# def save_writer(nam):
+#     name_file = get_town_in_file() + '.txt'
+#     with open(f'{name_file}', 'a', encoding='utf-8', errors='ignore') as file:
+#         file.write(nam + '\n\n' + '-----------------------------------------------------------' + '\n\n')
 
 
 def get_html_site(list_urls):
@@ -88,23 +97,51 @@ def get_html_site(list_urls):
                 opts.headless = True
                 assert opts.headless
                 driver = webdriver.Firefox(options=opts)
+                # driver = webdriver.Firefox()
                 driver.get(row_url)
-                time.sleep(7)
+                time.sleep(9)
             except Exception as e:
                 print(e)
                 continue
             try:
-                nam = driver.find_element_by_xpath('/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[9]')
-                # adress = driver.find_element_by_xpath(
-                #     '/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]')
+                main_info = driver.find_element_by_xpath(
+                    '/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[9]').text
+
+                main_info_dubler = driver.find_element_by_xpath(
+                    '/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[7]').text
+                name_org = driver.find_element_by_xpath(
+                    '/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]').text
+
+                row_phone = re.findall(r"(\+7|8).*?(\d{3}).*?(\d{3}).*?(\d{2}).*?(\d{2})", main_info)
+                row_phone2 = re.findall(r"(\+7|8).*?(\d{3}).*?(\d{3}).*?(\d{2}).*?(\d{2})", main_info_dubler)
+                site = re.findall(r'.+\.[a-zA-Z]{2,4}', main_info)
+                site2 = re.findall(r'.+\.[a-zA-Z]{2,4}', main_info_dubler)
+                #
+                # phone = str(''.join(row_phone)) + str(''.join(row_phone2))
+                # url0 = str(''.join(site)) + str(''.join(site2))
+
+                # if not phone:
+                #     phone = 'Телефон Не указан'
+                # elif not url0:
+                #     url0 = 'Сайт не указан'
+
                 print('------------------------------------------------')
-                print(nam.text)
+                print(f'Название " {name_org} "' + '\n')
+                print(main_info)
+                print(main_info_dubler)
+                print('tel', row_phone)
+                print('site  ', site)
                 print('------------------------------------------------')
                 print('')
-                save_writer(nam.text)
+
+                out_list = [name_org, row_phone, site]
+                save_in_csv(out_list)
+
                 driver.quit()
+
             except Exception as e:
-                print('nam', e)
+                print('007', e)
+                driver.close()
                 driver.quit()
                 continue
         else:
@@ -112,7 +149,7 @@ def get_html_site(list_urls):
 
 
 def main():
-    print('Это бета-версия 1.0')
+    print('Это бета-версия 1.1')
     print('Я работаю')
     try:
         get_html_site(run_browser(get_town_in_file(), get_categories_in_file()))
@@ -125,3 +162,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# <h1 jstcache="127" class="x3AX1-LfntMc-header-title-title gm2-headline-5" jsan="7.x3AX1-LfntMc-header-title-title,7.gm2-headline-5"> <span jstcache="128">Лавка странника</span> <span jstcache="129" class="x3AX1-LfntMc-header-title-haAclf"></span> </h1>
